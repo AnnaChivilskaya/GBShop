5cbe52f3ab47bcadc821e7789a9d306d771f4f13
Anna <kramannach@gmail.com>
2022-09-07 21:43:50 +0500

    1. Реализовать добавление товара в корзину. 2.Реализовать удаление товара из корзины. 3. Добавить в API метод payBasket, который будет имитировать списание денежных средств и обнуление корзины. Реализовать фронтенд функционала обращения к методу, покрыть его тестами при помощи mock-респонса.

---
+++ GBShop/App/BussinessLogic/Network/Cart/CartRequestFactory.swift
@@ -0,0 +1,34 @@
+//
+//  a.swift
+//  GBShopTests
+//
+//  Created by Аня on 07.09.2022.
+//
+
+import Foundation
+import Alamofire
+
+protocol CartRequestFactory {
+    
+    func addCart(
+        item: String,
+        price: Int,
+        amount: Int,
+        completionHandler: @escaping (AFDataResponse<AddCart>) -> Void)
+    
+    func makeCart(
+        item: String,
+        price: Int,
+        amount: Int,
+        bill: Int,
+        completionHandler: @escaping (AFDataResponse<BalanceCart>) -> Void)
+    
+    func removeCart(
+        item: String,
+        amount: Int,
+        completionHandler: @escaping (AFDataResponse<RemoveCart>) -> Void)
+    
+    func balanceCart(
+        result: Int,
+        completionHandler: @escaping (AFDataResponse<CartResult>) -> Void)
+}
